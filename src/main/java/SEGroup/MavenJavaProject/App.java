45fa910b387ede1cd6819012423a9aa9ba837f9e
tapasyamohan <kattatapasya@gmail.com>
2022-11-23 15:49:20 +0530

    Maven Java Project push

---
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "21BD5A6609 TAPASYA CSMB THIS IS MY FIRST JAVA PROJECT" );
+    }
+}
